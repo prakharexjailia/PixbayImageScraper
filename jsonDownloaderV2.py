92b7e695b6305a6d601dd772338ea2d144d5545f
Prakhar <prakharexjailia@gmail.com>
2019-04-11 21:14:56 +0530

    Update jsonDownloaderV2.py

--- jsonDownloaderV2.py
+++ jsonDownloaderV2.py
@@ -6,12 +6,13 @@ import sys    #Importing the System Library
 from urllib2 import Request,urlopen
 from urllib2 import URLError, HTTPError
 
-query = "sexy"
-pageCount = 30
-ImagePerPage = 50
+key = "a45646ad64a6d4a64d64ad" # put your key here in quotes get it from https://pixabay.com/api/docs/ under parameter by creating account.
+query = "Nature"               # your query
+pageCount = 30                 # query pages OPTIONAL
+ImagePerPage = 50              # no of image per page OPTIONAL
 count = 1
 while (count <= pageCount):
-	imageuri = "https://pixabay.com/api/?key=12161745-52de7fcb829b36a09827aa07e&q="+query+"&image_type=photo&page="+str(count)+"&per_page="+str(ImagePerPage) 
+	imageuri = "https://pixabay.com/api/?key="+key+"&q="+query+"&image_type=photo&page="+str(count)+"&per_page="+str(ImagePerPage) 
 	imageuri=imageuri.lower()
 	print(imageuri)
 	req = Request(imageuri, headers={"User-Agent": "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"})
